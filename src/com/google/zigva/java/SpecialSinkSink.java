7dc15c3f639aef010a53bd7b2dce11de0a8a7afd
zorzella <devnull@localhost>
2008-10-10 23:05:22 +0000

    Sink slightly integrated into Zystem

---
+++ src/com/google/zigva/java/SpecialSinkSink.java
@@ -0,0 +1,34 @@
+// Copyright 2008 Google Inc.  All Rights Reserved.
+package com.google.zigva.java;
+
+import com.google.zigva.io.DataSourceClosedException;
+import com.google.zigva.io.Sink;
+
+public class SpecialSinkSink<T> implements Sink<T> {
+
+  private final Sink<T> sink;
+//  private final Object lock;
+  
+  public SpecialSinkSink(Sink<T> source) { //, Object lock) {
+    this.sink = source;
+//    this.lock = lock;
+  }
+  
+  @Override
+  public void close() {
+//    synchronized (lock) {
+//      lock.notifyAll();
+//    }
+  }
+
+  @Override
+  public boolean isReady() throws DataSourceClosedException {
+    return sink.isReady();
+  }
+
+  @Override
+  public void write(T data) throws DataSourceClosedException {
+    sink.write(data);
+  }
+
+}
