cc6701e25a85fb405ee18b35292fde6265233205
zorzella <devnull@localhost>
2008-11-06 20:26:01 +0000

    Static

---
+++ src/com/google/zigva/sh/Static.java
@@ -0,0 +1,15 @@
+// Copyright 2008 Google Inc.  All Rights Reserved.
+package com.google.zigva.sh;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.zigva.guice.JavaModule;
+import com.google.zigva.guice.ZivaModule;
+
+public class Static {
+
+  public static final Injector injector = Guice.createInjector(
+      new ZivaModule (), 
+      new JavaModule());
+  
+}
