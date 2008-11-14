0a487737fef0ced0974e87953aa7c6c5d25421e6
zorzella <devnull@localhost>
2008-11-14 20:56:51 +0000

    Copyright notices, import cleanup

--- src/com/google/zigva/io/NullAppendable.java
+++ src/com/google/zigva/io/NullAppendable.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.zigva.io;
 
 import java.io.IOException;
@@ -8,15 +24,15 @@ public class NullAppendable implements Appendable{
 
   private NullAppendable(){}
   
-  public Appendable append(CharSequence csq) throws IOException {
+  public Appendable append(CharSequence csq) {
     return this;
   }
 
-  public Appendable append(CharSequence csq, int start, int end) throws IOException {
+  public Appendable append(CharSequence csq, int start, int end) {
     return this;
   }
 
-  public Appendable append(char c) throws IOException {
+  public Appendable append(char c) {
     return this;
   }
 
