5b7293179dcdc5dcdee48cbfac4e574341dd7c81
zorzella <devnull@localhost>
2009-01-07 22:12:05 +0000

    ZivaProcess/Builder/Util deleted

--- src/com/google/zigva/sh/ShellCommand.java
+++ src/com/google/zigva/sh/ShellCommand.java
@@ -19,11 +19,13 @@ package com.google.zigva.sh;
 import com.google.common.base.Join;
 import com.google.inject.Inject;
 import com.google.zigva.exec.CommandExecutor;
+import com.google.zigva.exec.Killable;
 import com.google.zigva.exec.ZigvaTask;
 import com.google.zigva.exec.CommandExecutor.Command;
 import com.google.zigva.guice.ZigvaThreadFactory;
 import com.google.zigva.java.io.ReaderSource;
 import com.google.zigva.java.io.Readers;
+import com.google.zigva.lang.ZigvaInterruptedException;
 import com.google.zigva.lang.Zystem;
 
 import java.io.IOException;
@@ -177,6 +179,61 @@ public class ShellCommand implements Command {
     return Join.join(" ", this.shellCommand);
   }
 
+  private static class ZivaProcess {
+
+    private final Process process;
+    private final Thread in;
+    private final Thread out;
+    private final Thread err;
+
+    public ZivaProcess(Process process, Thread in, Thread out, Thread err) {
+      this.process = process;
+      this.in = in;
+      this.out = out;
+      this.err = err;
+    }
+      
+    public void waitFor() {
+      try {
+        process.waitFor();
+        out.join();
+        if (err != null) {
+          err.join();
+        }
+        if (in != null) {
+          if (in.getState() != Thread.State.TERMINATED) {
+            in.interrupt();
+          }
+          in.join();
+        }
+      } catch (InterruptedException e) {
+        throw new ZigvaInterruptedException(e);
+      }
+    }
+
+    public int exitValue(){
+      return this.process.exitValue();
+    }
+    
+    public Process process() {
+      return this.process;
+    }
+
+    //TODO: Close input stream also?
+    public void kill() {
+      try {
+        //TODO: why do we need to do this?
+        this.process.getErrorStream().close();
+        this.process.getOutputStream().close();
+      } catch (IOException e) {
+        throw new Killable.FailedToKillException(e);
+      }
+      this.process.destroy();
+//      out.interrupt();
+//      err.interrupt();
+    }
+  }
+  
   private static class JavaProcess extends ZivaProcess {
     public JavaProcess(Process process, Thread in, Thread out, Thread err) {
       super(process, in, out, err);
